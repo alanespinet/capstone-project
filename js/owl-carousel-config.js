e665cfad8e9605dbb34e3262e1aa37bc9fa11585
aespinet <aelluvetaa@gmail.com>
2017-02-27 14:35:00 -0500

    prod02

---
+++ js/owl-carousel-config.js
@@ -0,0 +1,9 @@
+$(".owl-carousel").owlCarousel({
+  items: 1,
+  loop: true,
+  nav: true,
+  margin: 10
+});
+
+$('.owl-nav .owl-prev').html('<i class="fa fa-angle-double-left" aria-hidden="true"></i> &nbsp; previous work');
+$('.owl-nav .owl-next').html('next work &nbsp; <i class="fa fa-angle-double-right" aria-hidden="true"></i>');
